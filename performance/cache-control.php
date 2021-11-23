5b3c672dfac866b4ef30643c846441727f217599
Konstantin Kovshenin <kovshenin@gmail.com>
2021-11-23 13:42:54 +0300

    Cache: Add an hourly event to delete outdated entries.

--- performance/cache-control.php
+++ performance/cache-control.php
@@ -1,6 +1,74 @@
 <?php
 namespace Sail\Cache;
 
+// Runs in a CLI/Cron context, likely by-passing advanced-cache.php
+add_action( 'sail_cache_clean', function() {
+	$cache_dir = WP_CONTENT_DIR . '/cache/sail';
+	$start = microtime( true );
+	$keys = [];
+	$deleted = 0;
+	$time = time();
+
+	$levels = scandir( $cache_dir );
+	foreach ( $levels as $level ) {
+		if ( $level == '.' || $level == '..' ) {
+			continue;
+		}
+
+		if ( $level == 'flags.json' ) {
+			continue;
+		}
+
+		$items = scandir( "{$cache_dir}/{$level}" );
+		foreach ( $items as $item ) {
+			if ( $item == '.' || $item == '..' ) {
+				continue;
+			}
+
+			if ( substr( $item, -5 ) != '.meta' ) {
+				continue;
+			}
+
+			$cache_key = substr( $item, 0, -5 );
+			$keys[] = $cache_key;
+		}
+	}
+
+	foreach ( $keys as $cache_key ) {
+		$level = substr( $cache_key, -2 );
+
+		$f = fopen( "{$cache_dir}/{$level}/{$cache_key}.meta", 'r' );
+		if ( ! flock( $f, LOCK_EX ) ) {
+			// Could not acquire a lock.
+			fclose( $f );
+			continue;
+		}
+
+		$contents = '';
+		while ( ! feof( $f ) ) {
+			$contents .= fread( $f, 8192 );
+		}
+
+		$meta = json_decode( $contents, true );
+
+		// This cache entry is still valid.
+		if ( $meta && ! empty( $meta['expries'] ) && $meta['expires'] > $time ) {
+			fclose( $f );
+			continue;
+		}
+
+		// Delete the cache entry and release the lock.
+		unlink( "{$cache_dir}/{$level}/{$cache_key}.data" );
+		unlink( "{$cache_dir}/{$level}/{$cache_key}.meta" );
+		fclose( $f );
+		$deleted++;
+	}
+
+	$end = microtime( true );
+	$elapsed = $end - $start;
+	// printf( 'Deleted %d/%d keys in %.4f seconds' . PHP_EOL, $deleted, count( $keys ), $elapsed );
+} );
+
 // Bail if cache is inactive.
 if ( ! defined( '\Sail\Cache\CACHE_DIR' ) ) {
 	return;
@@ -27,6 +95,10 @@ add_action( 'clean_post_cache', function( $post_id, $post ) {
 }, 10, 2 );
 
 add_action( 'shutdown', function() {
+	if ( ! wp_next_scheduled( 'sail_cache_clean' ) ) {
+		wp_schedule_event( time(), 'hourly', 'sail_cache_clean' );
+	}
+
 	$expire = expire();
 	if ( empty( $expire ) ) {
 		return;
